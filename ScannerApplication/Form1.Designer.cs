717fea0894dc02684f93d57b0f945aa2be508008
SaiSindhuRudraraju <100202255+SaiSindhuRudraraju@users.noreply.github.com>
2022-02-22 16:30:16 +0530

    Add files via upload

---
+++ ScannerApplication/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ScannerApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonScan = new System.Windows.Forms.Button();
+            this.axFiScn1 = new AxFiScnLib.AxFiScn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axFiScn1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonScan
+            // 
+            this.buttonScan.Location = new System.Drawing.Point(106, 88);
+            this.buttonScan.Name = "buttonScan";
+            this.buttonScan.Size = new System.Drawing.Size(130, 35);
+            this.buttonScan.TabIndex = 0;
+            this.buttonScan.Text = "Scan";
+            this.buttonScan.UseVisualStyleBackColor = true;
+            this.buttonScan.Click += new System.EventHandler(this.buttonScan_Click);
+            // 
+            // axFiScn1
+            // 
+            this.axFiScn1.Enabled = true;
+            this.axFiScn1.Location = new System.Drawing.Point(31, 641);
+            this.axFiScn1.Name = "axFiScn1";
+            this.axFiScn1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axFiScn1.OcxState")));
+            this.axFiScn1.Size = new System.Drawing.Size(48, 48);
+            this.axFiScn1.TabIndex = 1;
+            this.axFiScn1.ScanToRawEx += new AxFiScnLib._DFiScnEvents_ScanToRawExEventHandler(this.AxFiScn1_ScanToRawEx);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.pictureBox1.Location = new System.Drawing.Point(409, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1116, 647);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1549, 701);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.axFiScn1);
+            this.Controls.Add(this.buttonScan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axFiScn1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonScan;
+        private AxFiScnLib.AxFiScn axFiScn1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ScannerApplication/Form1.cs
@@ -0,0 +1,47 @@
+﻿using ClassLibrary1;
+using FiScnUtildN;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScannerApplication
+{
+    public partial class Form1 : Form
+    {
+        ScannerConnection sc;
+        public Form1()
+        {
+            InitializeComponent();
+            sc= new ScannerConnection();
+        }
+
+        private void buttonScan_Click(object sender, EventArgs e)
+        {
+            sc.axFiScn1.ScanToRawEx += AxFiScn1_ScanToRawEx;
+            sc.ScanDocument(Handle.ToInt32());
+        }
+
+        private void AxFiScn1_ScanToRawEx(object sender, AxFiScnLib._DFiScnEvents_ScanToRawExEvent e)
+        {
+            Bitmap bitmap;
+            ConvH2BM Conv = new ConvH2BM();
+            bitmap = Conv.GetBitmapFromRAW(e.resolution, e.imageWidth, e.imageLength, e.bitPerPixel, e.compressionType, e.size, e.raw);
+            pictureBox1.Image = bitmap;
+        }
+
+        public String strFileName; //Holds scanned file name
+
+        
+        
+        private void axFiScn1_DetectBarcode(object sender, AxFiScnLib._DFiScnEvents_DetectBarcodeEvent e)
+        {
+            MessageBox.Show("DetectBarcode event is detected.\nNumber of scanned pages: " + e.readCount + "  page(s)" + "\nFile name: " + strFileName + "\nRecognized characters: " + e.barcodeText);
+        }
+    }
+}
